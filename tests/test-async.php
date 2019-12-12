23ec7b41e66ea16674eb06d8637251e1a1feb980
Dave Mackey <dave@davemackey.net>
2019-12-12 11:22:51 -0500

    - Moved PHPUnit tests towards actually working. Boy, that was a pain!!

--- tests/test-async.php
+++ tests/test-async.php
@@ -1,9 +1,5 @@
 <?php
 
-namespace LiquidChurch\LqdMessages {
-
-	use PHPUnit\Framework\TestCase;
-
 	class GCS_Async_Test extends WP_UnitTestCase {
 
 		function test_sample() {
@@ -15,8 +11,8 @@ namespace LiquidChurch\LqdMessages {
 			$this->assertTrue( class_exists( 'GCS_Async') );
 		}
 
-		function test_class_access() {
-			$this->assertInstanceOf( GCS_Async::class, gc_sermons()->async );
+		function test_class_access()
+		{
+			$this->assertInstanceOf(GCS_Async::class, gc_sermons()->async);
 		}
-	}
 }
