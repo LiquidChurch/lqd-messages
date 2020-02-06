9da690285c1736ef29eb4a1947ccad435ce53766
Dave Mackey <dave@davemackey.net>
2020-02-06 13:15:40 -0500

    - Further class renaming:  - GCSS_Recent_Series_Admin --> LqdM_Recent_Series_Admin  - GCSS_Recent_Admin_Base   --> LqdM_Recent_Admin_Base  - GCSS_Recent_Series_Run   --> LqdM_Recent_Series_Run  - GCS_Shortcodes_Run_Base  --> LqdM_Shortcodes_Run_Base  - GCS_Shortcodes_Recent_Series --> LqdM_Shortcodes_Recent_Series  - GCS_Shortcodes_Base      --> LqdM_Shortcodes_Base  - GCSS_Recent_Series_Run   --> LqdM_Recent_Series_Run  - GCSS_Recent_Series_Admin --> LqdM_Recent_Series_Admin - Further text-domain updates from gc-sermons to lqdm.

--- includes/shortcodes/shortcodes-recent-series/class-shortcode.php
+++ includes/shortcodes/shortcodes-recent-series/class-shortcode.php
@@ -4,7 +4,7 @@
  *
  * @package GC Sermons
  */
-class GCS_Shortcodes_Recent_Series extends GCS_Shortcodes_Base {
+class LqdM_Shortcodes_Recent_Series extends LqdM_Shortcodes_Base {
 	/**
 	 * Constructor
 	 *
@@ -13,8 +13,8 @@ class GCS_Shortcodes_Recent_Series extends GCS_Shortcodes_Base {
 	 * @return void
 	 */
 	public function __construct( $plugin ) {
-		$this->run   = new GCSS_Recent_Series_Run( $plugin->sermons );
-		$this->admin = new GCSS_Recent_Series_Admin( $this->run );
+		$this->run   = new LqdM_Recent_Series_Run( $plugin->sermons );
+		$this->admin = new LqdM_Recent_Series_Admin( $this->run );
 
 		parent::hooks();
 	}
