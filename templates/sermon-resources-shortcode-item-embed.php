0358ea9402a01b91991f589e7e8a20b5dd21f114
Dave Mackey <dave@davemackey.net>
2020-05-06 12:15:39 -0400

    - Cleaning out old files before merge.

--- templates/sermon-resources-shortcode-item-embed.php
+++
@@ -1,3 +0,0 @@
-<?php
-global $wp_embed, $content_width;
-echo $wp_embed->shortcode( $this->get( 'embed_args', array() ), esc_url( $this->get( 'file' ) ) );
--- templates/sermon-resources-shortcode-item-image.php
+++
@@ -1,9 +0,0 @@
-<a href="<?php $this->output( 'file', 'esc_url' ); ?>">
-	<img src="<?php $this->output( 'file', 'esc_url' ); ?>" alt="
-	<?php if ( $this->get( 'do_display_name' ) ) : ?>
-		<?php $this->output( 'display_name', 'esc_attr' ); ?>
-	<?php else : ?>
-		<?php $this->output( 'name', 'esc_attr' ); ?>
-	<?php endif; ?>
-	"/>
-</a>
